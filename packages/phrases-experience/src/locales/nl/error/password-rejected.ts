a93f0653fdee2d63824f0f0fbf5107bb7b516d78
marijnvanwezel <marijnvanwezel@gmail.com>
2025-01-14 21:59:16 +0100

    Translate error/password-rejected.ts

--- packages/phrases-experience/src/locales/nl/error/password-rejected.ts
+++ packages/phrases-experience/src/locales/nl/error/password-rejected.ts
@@ -9,17 +9,17 @@ type BreakdownKeysToObject<Key extends string> = {
 type RejectionPhrases = BreakdownKeysToObject<PasswordRejectionCode>;
 
 const password_rejected = {
-  too_short: 'Minimum length is {{min}}.',
-  too_long: 'Maximum length is {{max}}.',
-  character_types: 'At least {{min}} types of characters are required.',
-  unsupported_characters: 'Unsupported character found.',
-  pwned: 'Avoid using simple passwords that are easy to guess.',
-  restricted_found: 'Avoid overusing {{list, list}}.',
+  too_short: 'Minimale lengte is {{min}}.',
+  too_long: 'Maximale lengte is {{max}}.',
+  character_types: 'Tenminste {{min}} verschillende typen karakters zijn vereist.',
+  unsupported_characters: 'Niet-ondersteund teken gevonden.',
+  pwned: 'Gebruik geen wachtwoorden die makkelijk te raden zijn.',
+  restricted_found: 'Gebruik niet te veel van {{list, list}}.',
   restricted: {
-    repetition: 'repeated characters',
-    sequence: 'sequential characters',
-    user_info: 'your personal information',
-    words: 'product context',
+    repetition: 'herhaalde tekens',
+    sequence: 'sequentiële tekens',
+    user_info: 'je persoonlijke informatie',
+    words: 'productcontext',
   },
 } satisfies RejectionPhrases & {
   // Use for displaying a list of restricted issues
